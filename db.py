ed93f0018e96baa7f8a0b0352a511a775f4b757e
Julian L <julian.lawrance@gmail.com>
2023-02-17 20:42:09 +0000

    Add a few endpoints for getting hardcoded data

---
+++ db.py
@@ -0,0 +1,57 @@
+def get_games():
+    games = [
+        {
+            "id": "608ce9d2-41ce-4fb2-85ee-a7d5463709b1",
+            "name": "name-1",
+            "timeCreated": 1676659264,
+            "displayName": "Name 1",
+            "synopsis": "This is Name 1.",
+            "description": "",
+            "minPlayers": 2,
+            "maxPlayers": 4,
+            "winMethod": "individual-score",
+            "imageLink": "",
+            "links": [],
+        },
+    ]
+
+    return games
+
+def get_groups():
+    groups = [
+        {
+            "id": "120a6b3b-f519-4873-b04a-9e96b65c4280",
+            "name": "test-group",
+            "timeCreated": 1659775919,
+            "type": "public",
+            "displayName": "Test Group",
+            "description": "A test group",
+        },
+    ]
+
+    return groups
+
+def get_link_types():
+    link_types = [
+        {
+            "id" : "dd7265f2-44b4-4b96-9a9c-c2d7c84025c1",
+            "name" : "board-game-geek",
+            "timeCreated" : 1659777659,
+            "displayName" : "BoardGameGeek"
+        },
+    ]
+
+    return link_types
+
+def get_players():
+    players = [
+        {
+            "id" : "625b90d9-a918-4e91-b2ff-db16c48d828d",
+            "username" : "julianl",
+            "timeCreated" : 1659775610,
+            "displayName" : "Julian Lawrance",
+            "profilePicture" : "https://e.snmc.io/i/600/s/9f6d3d17acac6ce20993eb158c203e4b/5662600/godspeed-you-black-emperor-lift-yr-skinny-fists-like-antennas-to-heaven-cover-art.jpg"
+        },
+    ]
+
+    return players
--- function_app.py
+++ function_app.py
@@ -1,11 +1,42 @@
+import json
 import azure.functions as func
 import logging
 
+import db
+
 app = func.FunctionApp()
 
-# Learn more at aka.ms/pythonprogrammingmodel
+@app.function_name(name="GetGames")
+@app.route(route="games", auth_level=func.AuthLevel.ANONYMOUS)
+def get_games(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+        json.dumps(db.get_games()),
+        mimetype="application/json"
+    )
 
-# Get started by running the following code to create a function using a HTTP trigger.
+@app.function_name(name="GetGroups")
+@app.route(route="groups", auth_level=func.AuthLevel.ANONYMOUS)
+def get_groups(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+        json.dumps(db.get_groups()),
+        mimetype="application/json"
+    )
+
+@app.function_name(name="GetLinkTypes")
+@app.route(route="linkTypes", auth_level=func.AuthLevel.ANONYMOUS)
+def get_link_types(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+        json.dumps(db.get_link_types()),
+        mimetype="application/json"
+    )
+
+@app.function_name(name="GetPlayers")
+@app.route(route="players", auth_level=func.AuthLevel.ANONYMOUS)
+def get_players(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+        json.dumps(db.get_players()),
+        mimetype="application/json"
+    )
 
 @app.function_name(name="HttpTrigger1")
 @app.route(route="hello", auth_level=func.AuthLevel.ANONYMOUS)
